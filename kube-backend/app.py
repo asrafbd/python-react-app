f3529b96dd651dccedbc0a48356f0f4f1f978fbd
Md. Amdadul Bari Imad <amdadulbari@gmail.com>
2023-03-10 12:38:05 +0600

    db ops implemented

--- kube-backend/app.py
+++ kube-backend/app.py
@@ -1,26 +1,51 @@
 from flask import Flask,jsonify,request
+import psycopg2
+from dotenv import load_dotenv
+import os
+
 app = Flask(__name__)
+
+# set up connection to PostgreSQL database
+database_name = os.getenv('DATABASE_NAME')
+database_user = os.getenv('DATABASE_USER')
+database_password = os.getenv('DATABASE_PASSWORD')
+database_host = os.getenv('DATABASE_HOST')
+database_port = os.getenv('DATABASE_PORT')
+
+conn = psycopg2.connect(
+    database=database_name,
+    user=database_user,
+    password=database_password,
+    host=database_host,
+    port=database_port
+)
 
 @app.route('/')
 def hello_world():
     return 'Hello world'
 
-
-@app.route('/reverser',methods = ['POST'])
-def reverser():
-    num = request.get_json().get("num")
-    num = int(num[len(num)::-1])
-    return jsonify({"num":num})
-
-
 @app.route('/summation',methods = ['POST'])
 def summation():
-    num = request.get_json().get("num")
-    sum = 0
-    for i in num:
-        sum += int(i)
+    num = request.json['num']
+    cur = conn.cursor()
+    cur.execute("select sum(balance) from balance where num=%s", (num,))
+    row = cur.fetchone()
+    if row is None:
+        return jsonify({'sum': 'User not found'})
+    else:
+        return jsonify({'sum': row[0]})
 
-    return jsonify({"sum":sum})
+@app.route('/reverser', methods=['POST'])
+def get_user_contact():
+    num = request.json['num']
+    cur = conn.cursor()
+    cur.execute("SELECT name FROM users WHERE number=%s", (num,))
+    row = cur.fetchone()
+
+    if row is None:
+        return jsonify({'num': 'User not found'})
+    else:
+        return jsonify({'num': row[0]})
 
 if __name__ == "__main__":
      app.run()
